0bca205bc57172ff38e371917c96597b45678132
Marko Berger <marko.berger@gmail.com>
2018-03-09 22:23:22 +0100

    Add Patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const {ObjectId} = require('mongodb');
 
@@ -70,6 +71,30 @@ app.delete('/todos/:id',(req, res)=>{
     });
 })
 
+app.patch('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else {
+        body.completed = false; 
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+
+})
 
 app.listen(port, ()=>{
     console.log(`Server started at PORT ${port}`);
